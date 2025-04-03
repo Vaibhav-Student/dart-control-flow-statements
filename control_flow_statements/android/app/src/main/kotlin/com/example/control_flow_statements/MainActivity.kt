2877c5987338fa8d1185931f6e61658e18b0ff00
Vaibhav-Student <vaibhavbeladiya1112@gmail.com>
2025-04-03 16:58:35 +0530

    write condition statements.

---
+++ control_flow_statements/android/app/src/main/kotlin/com/example/control_flow_statements/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.control_flow_statements
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- control_flow_statements/lib/main.dart
+++ control_flow_statements/lib/main.dart
@@ -1,6 +1,71 @@
+// void main() {
+//   int number = 10;
+//   if (number > 0) {
+//     print("Number is positive.");
+//   }
+// }
+
+// void main() {
+//   int n1 = -5;
+//   if (n1 > 0) {
+//     print("Number is positive");
+//   }else {
+//     print("Number is negative");
+//   }
+// }
+
+// void main() {
+//   int n1 = 0 ;
+//   if (n1 > 0) {
+//     print("Number is positive");
+//   } else if (n1 < 0) {
+//     print("Number is negative");
+//   } else {
+//     print("Number is zero");
+//   }
+// }
+
+// void main() {
+//   String grade = 'C';
+//   switch (grade) {
+//     case 'A':
+//       print("Excellent!");
+//       break;
+//     case 'B':
+//       print("Good Job!");
+//       break; 
+//     case 'C':
+//       print("Needs Improvement");
+//       break;
+//     default:
+//       print("Invalid Grade"); 
+//   }
+// }
+
+// void main() {
+//   int number = 5;
+//   String result = (number % 2 == 0) ? "Even" : "Odd";
+//   print(result);
+// }
+
+// void main() {
+//   String? name;
+//   print(name ?? "Default name");
+
+//   name ??= "John";
+//   print(name);
+// }
+
 void main() {
-  int number = 10;
-  if (number > 0) {
-    print("Number is positive.");
+  dynamic value = "raj";
+  switch (value) {
+    case int():
+      print("It's an integer");
+      break;
+    case String():
+      print("It's a string");
+      break;
+    default:
+      print("Unknown type");  
   }
-}   
+}
